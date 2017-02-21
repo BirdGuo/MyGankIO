4a79dd09822c0b12112c2e182b64558c6d006cd6
浪里小白蛇 <gxw603004002@126.com>
2017-02-21 17:08:09 +0800

    添加详情页

---
+++ GankIO/pages/specific/specific.js
@@ -0,0 +1,30 @@
+Page({
+	data:{
+		hidden:false,
+		toastHidden:true,
+		modalHidden:true,
+		   toastText: "数据无法正常显示，请将此问题上报管理员进行处理",
+        loadingText: "加载中..."
+	},
+	onLoad:function(options){
+		
+	},
+	onReady:function(){
+		
+	},
+	onShow:function(){
+		
+	},
+	onHide:function(){
+		
+	},
+	onUnload:function(){
+		
+	},
+	onPullDownRefresh:function(){
+		
+	},
+	onReachBottom:function(){
+		
+	}
+})		
